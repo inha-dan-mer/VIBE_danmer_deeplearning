e2ba6b432d5b89ddcf0e1eb46202a95bfd05741e
SH Kim <62065749+ksh7621@users.noreply.github.com>
2022-06-03 13:29:17 +0900

    Update demo.py

--- demo.py
+++ demo.py
@@ -263,7 +263,7 @@ def main(args):
     fps = num_frames / (end - vibe_time)
     
     print(joints2d_img_coord)
-    np.save('./save_tutor_coordin', joints2d_img_coord)
+    np.save('./save_coordin', joints2d_img_coord)
 
     print(f'VIBE FPS: {fps:.2f}')
     total_time = time.time() - total_time
